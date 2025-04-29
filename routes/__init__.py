a828c0c6b44bea7cc7e75853e32f02515eee5004
khiemnq <148186063+qtpyke24@users.noreply.github.com>
2025-04-29 16:27:58 +0700

    Create __init__.py

---
+++ routes/__init__.py
@@ -0,0 +1,9 @@
+from .messages import messages_bp
+from .contacts import contacts_bp
+from .notifications import notifications_bp
+
+__all__ = [
+    'messages_bp',
+    'contacts_bp',
+    'notifications_bp'
+]
