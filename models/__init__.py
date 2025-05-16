6e24b6e828b703749ecc9a2934da62072191196c
khiemnq <148186063+qtpyke24@users.noreply.github.com>
2025-05-16 10:09:33 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .message import Message
 from .user import User
 from .contact import Contact
+from .notification import Notification
 
 __all__ = [
     'Message',
     'User',
-    'Contact'
+    'Contact',
+    'Notification'
 ]
