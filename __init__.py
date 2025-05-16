15de0c8d6cded5de8a70726eee22b84fec9b90d0
khiemnq <148186063+qtpyke24@users.noreply.github.com>
2025-05-16 10:05:02 +0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .messages import messages_bp
-from .contacts import contacts_bp
-from .notifications import notifications_bp
-
-__all__ = [
-    'messages_bp',
-    'contacts_bp',
-    'notifications_bp'
-]
